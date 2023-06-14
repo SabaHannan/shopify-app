3c47a2792300063a32ecdd46148db21d728a1288
SabaHannan <sabahannan27@gmail.com>
2023-06-14 15:17:29 +0200

    Frontend changes for rendering DropZone component

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -23,8 +23,8 @@ export default function App() {
             <NavigationMenu
               navigationLinks={[
                 {
-                  label: t("NavigationMenu.pageName"),
-                  destination: "/pagename",
+                  label: t("NavigationMenu.newPage"),
+                  destination: "/carousel/new",
                 },
               ]}
             />
