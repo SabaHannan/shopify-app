81ec54bf958df613778948628e969a2ac3f9eb55
Stephen Masunga <steven.masunga@gmail.com>
2023-06-19 14:10:09 +0200

    Picture model file updated for sequelize

--- backend/src/models/picture.Model.ts
+++ backend/src/models/picture.Model.ts
@@ -1,6 +1,40 @@
-export interface picture {
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../sequelize";
+
+interface pictureAttributes {
     pictureID?: number,
     pictureName?: string,
     pictureData?: any
 }
 
+class picture extends Model<pictureAttributes> implements pictureAttributes {
+    public pictureID?: number;
+    public pictureName?: string;
+    public pictureData?: any
+}
+
+picture.init({
+    pictureID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        autoIncrementIdentity: true
+    },
+    pictureName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    pictureData: {
+        type: DataTypes.ARRAY(DataTypes.BLOB),
+        allowNull: true
+    }
+},
+{
+    sequelize,
+    modelName: 'picture',
+    tableName: 'picture',
+    timestamps: false
+}
+);
+
+export default picture;
