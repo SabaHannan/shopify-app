155179b7ef126bf6ce053787ed164d34591db34d
SabaHannan <sabahannan27@gmail.com>
2023-06-26 14:27:25 +0200

    Added buttons for embedding

--- web/frontend/pages/carousel/imageCarousel.jsx
+++ web/frontend/pages/carousel/imageCarousel.jsx
@@ -2,26 +2,44 @@ import {
     Layout,
     Page,
     Text,
+    Button,
   } from "@shopify/polaris";
 // import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation } from "react-i18next";
 import { SlickImages } from '../../components/SlickImages';
 
+// Page that a person can edit and see one of their carousels.
 const imageCarousel = () => {
   // TRANSLATION
   const { t } = useTranslation();
   
     return (
-        <Page title={t("NavigationMenu.imageCarousel")}>
-            <Layout>
-                <Text>
-                    "Lorem Ipsum..."
-                </Text>
-            </Layout>
-                <div>
-                    {/* Calling the slick slider to render carousel here */}
-                    <SlickImages />
-                </div>
+        <Page title={t("NavigationMenu.imageCarousel")}
+        primaryAction={
+            <Button
+              primary
+              connectedDisclosure={{
+                accessibilityLabel: 'Other embed actions',
+                actions: [{content: 'Copy code'}],
+              }}
+              //   Handle the embed y opening the theme editor 
+              //   onClick={() => handleEmbed(carousel.id)}
+            >
+              Embed
+            </Button>
+          }
+              secondaryActions={[
+                {
+                    content: 'Delete',
+                    destructive: 'true',
+                    helpText: 'Deleting the carousel cannot be undone',
+                },
+              ]}
+        >
+            <div style={{marginTop: "20px",}}>
+                {/* Calling the slick slider to render carousel here */}
+                <SlickImages />
+            </div>
         </Page>
     );
 }
