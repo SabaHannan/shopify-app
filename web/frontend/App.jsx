db84092e048909a8947a88c52ec339b941b0173a
Stephen Masunga <steven.masunga@gmail.com>
2023-06-30 13:21:40 +0200

    Apollo client and provider in the root of the app, app running fine

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -8,6 +8,7 @@ import {
   QueryProvider,
   PolarisProvider,
 } from "./components";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 export default function App() {
   // Any .tsx or .jsx files in /pages will become a route
@@ -15,25 +16,36 @@ export default function App() {
   const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
   const { t } = useTranslation();
 
+  //Declaring the backend connection URI
+  const ENDPOINT_URI = 'http://localhost:3000';
+
+  //Creating the Apollo client instance
+  const client = new ApolloClient({
+      uri: ENDPOINT_URI,
+      cache: new InMemoryCache()
+  });
+
   return (
     <PolarisProvider>
       <BrowserRouter>
         <AppBridgeProvider>
-          <QueryProvider>
-            <NavigationMenu
-              navigationLinks={[
-                // {
-                //   label: t("NavigationMenu.newPage"),
-                //   destination: "/carousel/new",
-                // },
-                // {
-                //   label: t("NavigationMenu.imageCarousel"),
-                //   destination: "/carousel/imageCarousel",
-                // },
-              ]}
-            />
-            <Routes pages={pages} />
-          </QueryProvider>
+          <ApolloProvider client={client}>
+            <QueryProvider>
+              <NavigationMenu
+                navigationLinks={[
+                  // {
+                  //   label: t("NavigationMenu.newPage"),
+                  //   destination: "/carousel/new",
+                  // },
+                  // {
+                  //   label: t("NavigationMenu.imageCarousel"),
+                  //   destination: "/carousel/imageCarousel",
+                  // },
+                ]}
+              />
+              <Routes pages={pages} />
+            </QueryProvider>
+          </ApolloProvider>  
         </AppBridgeProvider>
       </BrowserRouter>
     </PolarisProvider>
